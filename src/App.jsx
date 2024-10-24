e76181233d0c8ceecbc5c176faa8e0c91c1acb36
Kavinder Singh <kavinder.singh.3702@gmail.com>
2024-10-24 11:29:50 +0530

    carasouel

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
 function App() {
   var settings = {
-    dots: true,
+    // dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 1,
     slidesToScroll: 1,
+    nextArrow: <CustomNextArrow />,
+    prevArrow: <CustomPrevArrow />,
   };
   return (
     <>
       <Slider {...settings}>
         {data.map((d, index) => (
-          <div
-            key={index}
-            className="w-full h-full flex justify-center items-center"
-            style={{
-              backgroundImage: `url(${d.background})`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
-              minHeight: "500px",
-            }}
-          >
-            <img className="w-[20vw]" src={d.img} alt="image" />
+          <div key={index} className="border border-white flex justify-center ">
+            <img
+              src={d.background}
+              alt="background"
+              className="z-0 h-screen w-full object-cover"
+            />
+            <img
+              src={d.img}
+              alt=""
+              className="z-50 top-0 h-[100%] w-[100vw] justify-center items-center absolute "
+            />
           </div>
         ))}
       </Slider>
@@ -63,3 +66,29 @@ const data = [
   },
 ];
 export default App;
+const CustomNextArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div
+      className="absolute right-0 text-white cursor-pointer bg-yellow-500 p-3 rounded-full"
+      style={{ zIndex: 1, top: "50%" }}
+      onClick={onClick}
+    >
+      <FaArrowRight size={20} />
+    </div>
+  );
+};
+
+// Custom Previous Arrow component
+const CustomPrevArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div
+      className="absolute left-0 text-white cursor-pointer bg-yellow-500 p-3 rounded-full"
+      style={{ zIndex: 1, top: "50%" }}
+      onClick={onClick}
+    >
+      <FaArrowLeft size={20} />
+    </div>
+  );
+};
